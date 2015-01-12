c83d17194c84a3f220c3e1970c1ae459e37a8a07
wulfthor <thorwulf@hotmail.com>
2015-01-12 18:20:01 +0100

    mailqueue check

---
+++ NagiosMonitoring_ExchangeQueueHealth.ps1
@@ -0,0 +1,78 @@
+##############################################################################
+#
+# NAME: 	check_mailq.ps1
+#
+# AUTHOR: 	
+# EMAIL: 	
+#
+# COMMENT:  
+#
+#			Return Values for NRPE:
+#			Status optimal - OK (0)
+#			Status suboptimal - WARNING (1)
+#			Status degraded - CRITICAL (2)
+#
+##############################################################################
+
+$returnStateOK = 0
+$returnStateWarning = 1
+$returnStateCritical = 2
+$returnStateUnknown = 3
+
+
+
+if ( (Get-PSSnapin -Name Microsoft.Exchange.Management.PowerShell.E2010 -ErrorAction:SilentlyContinue) -eq $null)
+{
+    Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+}
+
+$NagiosStatus = "0"
+
+
+#$resObj=Get-Queue -Server MAIL-01 | ForEach-Object { Write-Host -Separator ";" $_.Identity,$_.NextHopDomain,$_.MessageCount }
+  
+$resultCount = Get-Queue -Server MAIL-01 | Measure-Object | Select-Object -expand Count
+[string[]]$resultHop = Get-Queue -Server MAIL-01 | Select-Object Identity,NextHopDomain,MessageCount
+
+$NagiosStatus = "0"
+$NagiosDescription = ""
+$NagiosPerfData = ""
+
+$NagiosDescription = Get-Queue -Server MAIL-01 | ForEach-Object { Write-Output -Message " "  $_.Identity " " $_.MessageCount  " msgto "  $_.NextHopDomain " "
+}
+
+for ($i=0;$i -lt $resultHop.length;$i++) 
+{
+    $tmpString = $resultHop[$i].Split(";")
+					# Format the output for Nagios
+					if ($NNagiosDescription -ne "") 
+					{
+						# we already have a message queue description, so we will add a separator
+						$NNagiosDescription = $NNagiosDescription + ", " 	
+					}
+					$NNagiosDescription = $NNagiosDescription + $tmpString[0] + " queue has " + $tmpString[1] + " messages to " + $tmpString[2]
+			
+		
+				# Look for lagged queues - warning if over 2
+}
+
+
+# Output, what level should we tell our caller?
+$NagiosPerfData = "|queue=" + $resultCount + ";10;20;0"
+
+if ($resultCount -gt "20") 
+{
+	Write-Host "CRITICAL: " $NagiosDescription" "$NagiosPerfData
+  NagiosStatus = "2"
+} 
+elseif ($resultCount -gt "15")
+{
+	Write-Host "WARNING: " $NagiosDescription" "$NagiosPerfData
+  $NagiosStatus = "1"
+} 
+else 
+{
+	Write-Host "OK: All mail queues " $NagiosDescription " within limits. "$NagiosPerfData
+}
+
+exit $NagiosStatus		
